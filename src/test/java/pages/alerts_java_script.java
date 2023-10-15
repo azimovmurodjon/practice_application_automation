c45c0849042c803a282b158ebcded9075aa05af5
Iryna Azimova <iryna.azimova@gmail.com>
2023-10-15 18:31:38 -0400

    Adding   @HTML_Tag_Examples and @Alerts

---
+++ src/test/java/pages/alerts_java_script.java
@@ -0,0 +1,9 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class alerts_java_script {
+    public alerts_java_script(WebDriver driver){
+
+    }
+}
---
+++ src/test/java/pages/dynamic_table_test_page.java
@@ -0,0 +1,9 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class dynamic_table_test_page {
+    public dynamic_table_test_page(WebDriver driver){
+
+    }
+}
